edf15cd0cf89764ee36a764bf20e97af0f6882bb
Christian Perez <perezchristian1012@gmail.com>
2019-10-02 11:33:48 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -21,7 +21,7 @@ class Dog
     @@all.clear
   end
 
-  def self.print_all
-    self.all.each{|dog| puts "#{dog.name}"}
-  end
+  # def self.print_all
+  #   self.all.each{|dog| puts "#{dog.name}"}
+  # end
 end
